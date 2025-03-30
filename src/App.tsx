972341b1f1bce2147039536f78fdfbd840dbfd2b
amen ramadan <amen@MacbookShops-MacBook-Pro.local>
2025-03-30 08:07:12 +0300

    Resizable Panel Component

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,21 @@ import "./App.css";
 import RecursiveComponent from "./components/RecursiveComponent";
 import { fileTree } from "./data/fileTree";
 import OpenedFilesBar from "./components/OpenedFilesBar";
+import ResizablePanel from "./components/ResizablePanel";
 
 function App() {
   return (
     <div className="">
       <div className="flex h-screen">
-        <div className="w-64 border-r p-2  border-b-[1px] border-[#ffffff1f]">
-          <RecursiveComponent fileTree={fileTree} />
-        </div>
-        <OpenedFilesBar />
+        <ResizablePanel
+          leftPanel={
+            <div className="w-64 p-2">
+              <RecursiveComponent fileTree={fileTree} />
+            </div>
+          }
+          rightPanel={<OpenedFilesBar />}
+          showLeftPanel
+        />
       </div>
     </div>
   );
